7f87fae40aaf382163a2b2de3520c3b08702494c
Saurabh Kumar <saurabh.cse04@gmail.com>
2019-11-10 18:46:06 +0530

    added new file

---
+++ broadcast_msg_to_all_workers.js
@@ -0,0 +1,54 @@
+//cluster.js
+const cluster = require('cluster');
+const os = require('os');
+
+ //*** mock DB
+  const numberOfUsersInDB = function(){
+    this.count = this.count || 5;
+    this.count = this.count * this.count;
+    return this.count;
+  }
+  //****
+
+if(cluster.isMaster){
+  const cpus = os.cpus().length;
+  console.log(`Forking for ${cpus} CPU`);
+  for(let i =0; i<cpus;i++){
+    cluster.fork();
+  } 
+
+const updateWorkers = () => {
+  const userCount = numberOfUsersInDB();
+  Object.values(cluster.workers).forEach((worker)=>{
+    worker.send({userCount})
+  })
+}
+updateWorkers();
+setInterval(updateWorkers,10000);
+
+  // console.dir(cluster.workers,{depth:0});
+  /*Object.values(cluster.workers).forEach((worker)=>{
+    worker.send(`Hello worker ${worker.id}`)
+  })*/
+}else{
+  require(`./server`);
+}
+
+//server.js
+const http = require('http');
+const pid = process.pid;
+
+let userCount;
+
+http.createServer((req,res)=>{
+  for (let i=0 ;i < 1e7; i++); // simulate CPU work
+    res.write(`handled by process ${pid}\n`);
+  	res.end(`users :  ${userCount}`)
+}).listen(3000,(err)=>{
+  console.log('server start on 3000');
+  console.log(`started process ${pid}`);
+});
+
+process.on('message',msg => {
+  userCount : msg.userCount;
+});
